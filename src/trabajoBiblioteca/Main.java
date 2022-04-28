d0d3b6bb9e3b77cebf8f58e1eb0da41bee81ce03
alvaroSaavedraCalero <91503108+alvaroSaavedraCalero@users.noreply.github.com>
2022-04-28 17:28:48 +0200

    Clase Cliente cuasiTerminada

--- src/trabajoBiblioteca/Main.java
+++ src/trabajoBiblioteca/Main.java
@@ -11,7 +11,6 @@ public class Main {
     public static void main(String[] args) {
         Almacen almacen = new Almacen();
         Cliente cliente = (Cliente) almacen.getAlmacenPersonas().get(1);
-        cliente.reservarDocumento(almacen.getAlmacenDocumentos());
-        System.out.println(cliente.getDocumentosReservados());
+        cliente.consultarTodosDocumentos(almacen.getAlmacenDocumentos());
     }
 }
--- src/trabajoBiblioteca/personas/Cliente.java
+++ src/trabajoBiblioteca/personas/Cliente.java
@@ -67,6 +67,10 @@ public class Cliente extends Persona implements InterfaceCliente {
                 '}';
     }
 
+    /**
+     * Filtra entre tres campos el documento que el usuario quiere reservar
+     * @param almacenDocumentos Almacen donde se encuentra el documento
+     */
     @Override
     public void reservarDocumento(ArrayList<Documento> almacenDocumentos) {
         Iterator<Documento> iterator = almacenDocumentos.iterator();
@@ -76,6 +80,7 @@ public class Cliente extends Persona implements InterfaceCliente {
             System.out.println("1. Nombre");
             System.out.println("2. Id");
             System.out.println("3. Editorial");
+            System.out.println("4. Salir");
             Integer respuesta = Funciones.pedirNumero();
             switch (respuesta) {
                 case 1:
@@ -94,7 +99,6 @@ public class Cliente extends Persona implements InterfaceCliente {
                             System.out.println("No se ha encontrado ningun Documento con ese nombre");
                         }
                     }
-                    continuar = false;
                     break;
 
                 case 2:
@@ -113,7 +117,6 @@ public class Cliente extends Persona implements InterfaceCliente {
                             System.out.println("No se ha encontrado ningun Documento con ese Id");
                         }
                     }
-                    continuar = false;
                     break;
 
                 case 3:
@@ -132,8 +135,11 @@ public class Cliente extends Persona implements InterfaceCliente {
                             System.out.println("No se ha encontrado ningun Documento con ese Id");
                         }
                     }
-                    continuar = false;
                     break;
+
+                case 4:
+                    System.out.println("Saliendo...");
+                    continuar = false;
 
                 default:
                     System.out.println("No ha escrito un numero correcto.");
@@ -143,6 +149,10 @@ public class Cliente extends Persona implements InterfaceCliente {
         }
     }
 
+    /**
+     * Filtra entre tres campos el documento que el usuario quiere devolver
+     * @param almacenDocumentos Almacen donde se encuentra el documento
+     */
     @Override
     public void devolverDocumento(ArrayList<Documento> almacenDocumentos) {
         Iterator<Documento> iterator = almacenDocumentos.iterator();
@@ -152,6 +162,7 @@ public class Cliente extends Persona implements InterfaceCliente {
             System.out.println("1. Nombre");
             System.out.println("2. Id");
             System.out.println("3. Editorial");
+            System.out.println("4. Salir");
             Integer respuesta = Funciones.pedirNumero();
             switch (respuesta) {
                 case 1:
@@ -170,7 +181,6 @@ public class Cliente extends Persona implements InterfaceCliente {
                             System.out.println("No se ha encontrado ningun Documento con ese nombre");
                         }
                     }
-                    continuar = false;
                     break;
 
                 case 2:
@@ -189,7 +199,6 @@ public class Cliente extends Persona implements InterfaceCliente {
                             System.out.println("No se ha encontrado ningun Documento con ese Id");
                         }
                     }
-                    continuar = false;
                     break;
 
                 case 3:
@@ -208,24 +217,107 @@ public class Cliente extends Persona implements InterfaceCliente {
                             System.out.println("No se ha encontrado ningun Documento con ese Id");
                         }
                     }
-                    continuar = false;
                     break;
 
+                case 4:
+                    System.out.println("Saliendo...");
+                    continuar = false;
+                    break;
                 default:
                     System.out.println("No ha escrito un numero correcto.");
+                    break;
             }
 
 
         }
     }
 
+    /**
+     * Filtra entre tres campos y muestra la informacion del documento
+     * @param almacenDocumentos Almacen donde se encuentra el documento
+     */
     @Override
     public void consultarDocumento(ArrayList<Documento> almacenDocumentos) {
+        Iterator<Documento> iterator = almacenDocumentos.iterator();
+        boolean continuar = true;
+        while (continuar) {
+            System.out.println("¿Por que campo quiere consultar el documento?");
+            System.out.println("1. Nombre");
+            System.out.println("2. Id");
+            System.out.println("3. Editorial");
+            System.out.println("4. Salir");
+            Integer respuesta = Funciones.pedirNumero();
+            switch (respuesta) {
+                case 1:
+                    System.out.println("Cual es el nombre del Documento?");
+                    String nombre = entrada.nextLine().toUpperCase();
+                    boolean continuar1 = true;
+                    while (iterator.hasNext() && continuar1) {
+                        Documento doc = iterator.next();
+                        if (doc.getNombre().equals(nombre)) {
+                            System.out.println(doc);
+                            continuar1 = false;
+                        }
+                        if (iterator.hasNext() == false) {
+                            System.out.println("No se ha encontrado ningun Documento con ese nombre");
+                        }
+                    }
+                    break;
 
+                case 2:
+                    System.out.println("Cual es el Id del Documento?");
+                    Integer id = Funciones.pedirNumero();
+                    boolean continuar2 = true;
+                    while (iterator.hasNext() && continuar2) {
+                        Documento doc = iterator.next();
+                        if (doc.getId().equals(id)) {
+                            System.out.println(doc);
+                            continuar2 = false;
+                        }
+                        if (iterator.hasNext() == false) {
+                            System.out.println("No se ha encontrado ningun Documento con ese Id");
+                        }
+                    }
+                    break;
+
+                case 3:
+                    System.out.println("Cual es la Editorial del Documento?");
+                    String editorial = entrada.nextLine().toUpperCase();
+                    boolean continuar3 = true;
+                    while (iterator.hasNext() && continuar3) {
+                        Documento doc = iterator.next();
+                        if (doc.getEditorial().equals(editorial)) {
+                            System.out.println(doc);
+                            continuar3 = false;
+                        }
+                        if (iterator.hasNext() == false) {
+                            System.out.println("No se ha encontrado ningun Documento con ese Id");
+                        }
+                    }
+                    break;
+
+                case 4:
+                    System.out.println("Saliendo...");
+                    continuar = false;
+                    break;
+                default:
+                    System.out.println("No ha escrito un numero correcto.");
+                    break;
+            }
+
+
+        }
     }
 
+    /**
+     * Muestra en pantalla la informacion de todo el almacen
+     * @param almacenDocumentos Almacen donde se encuentran los documentos
+     */
     @Override
     public void consultarTodosDocumentos(ArrayList<Documento> almacenDocumentos) {
-
+        for (Documento doc :
+             almacenDocumentos) {
+            System.out.println(doc);
+        }
     }
 }
